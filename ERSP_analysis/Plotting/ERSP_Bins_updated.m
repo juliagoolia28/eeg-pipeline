651bfcdee2a2eab204e50c9347bde2477253025a
juliagoolia28 <41306746+juliagoolia28@users.noreply.github.com>
2019-01-11 14:27:49 -0500

    Update ERSP_Bins_updated.m

--- ERSP_analysis/Plotting/ERSP_Bins_updated.m
+++ ERSP_analysis/Plotting/ERSP_Bins_updated.m
@@ -5,7 +5,7 @@ Summary of function:
 This script plots ERSP scalp map bins of specified widths for all electrodes.
 You need to have a study loaded first.
 
-Written by Vyom
+Written by Vyom Raval
 8/24/2018
 edited 1/9/2019
 %}
